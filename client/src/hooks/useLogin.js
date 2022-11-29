70fc74a377f3f885cbd1e56f7d1154b300b79438
Iyin0 <p2iyinoluwa@gmail.com>
2022-11-29 11:51:46 +0100

    fetch api change to test production deploy

--- client/src/hooks/useLogin.js
+++ client/src/hooks/useLogin.js
@@ -12,28 +12,53 @@ export const useLogin = () => {
         setIsLoading(true)
         setError(null)
 
-        const response = await fetch('/api/auth/login', {
+        // const response = await fetch('/api/auth/login', {
+        //     method: 'POST',
+        //     headers: { 'Content-Type': 'application/json' },
+        //     body: JSON.stringify({ email, password })
+        // })
+
+        // const json = await response.json()
+
+        // if (!response.ok) {
+        //     setIsLoading(false)
+        //     setError(json.error)
+        // }
+
+        // if (response.ok) {
+        // //  save the user to local storage
+        // localStorage.setItem('user', JSON.stringify(json))
+
+        // //  update the AuthContext
+        // dispatch({ type: 'LOGIN', payload: json })
+        // setIsLoading(false)
+        //     navigate('/welcome')
+        // }
+
+        await fetch('/api/auth/login', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ email, password })
         })
+            .then(res => {
+                return res.json()
+            })
+            .then(data => {
+                const { error } = data
+                if (error) {
+                    setError(error)
+                    setIsLoading(false)
+                }
+                else {
+                    //  save the user to local storage
+                    localStorage.setItem('user', JSON.stringify(data))
 
-        const json = await response.json()
-
-        if (!response.ok) {
-            setIsLoading(false)
-            setError(json.error)
-        }
-
-        if (response.ok) {
-            //  save the user to local storage
-            localStorage.setItem('user', JSON.stringify(json))
-
-            //  update the AuthContext
-            dispatch({ type: 'LOGIN', payload: json })
-            setIsLoading(false)
-            navigate('/welcome')
-        }
+                    //  update the AuthContext
+                    dispatch({ type: 'LOGIN', payload: data })
+                    setIsLoading(false)
+                    navigate('/welcome')
+                }
+            })
     }
 
     return { login, isLoading, error }
